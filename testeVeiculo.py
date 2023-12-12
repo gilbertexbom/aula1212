6c9feaccb8c8260c7408ac048473b276c89aa1d5
Instrutor <gilberto.braga0021@gmail.com>
2023-12-12 16:46:51 -0300

    - Complemento método toString

---
+++ testeVeiculo.py
@@ -0,0 +1,10 @@
+# Arquivo com código de teste da classe Veículo
+
+from veiculo import Veiculo
+
+minhaCaranga = Veiculo('Fiat', '147', 'Amarelo', 0)
+
+#Exibindo a minha caranga
+print('\n\t\t\t -- Minha Caranga --\n')
+print(minhaCaranga)
+
